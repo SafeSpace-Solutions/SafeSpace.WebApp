e030be1e4c3648681f13ab2c0e6e44fd3ef9754e
aditya <aditya.pradeepg2000@gmail.com>
2024-01-14 21:27:26 +0530

    app component and routing cleanup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,28 @@ import { authorizationGuard } from './shared/guards/authorization.guard';
 import { unauthorizationGuard } from './shared/guards/unauthorization.guard';
 
 const routes: Routes = [
-  { path: 'account', loadChildren: () => import('./account/account.module').then(module => module.AccountModule), canActivate: [unauthorizationGuard] },
-  {path: 'dashboard',  loadChildren: () => import('./dashboards/seeker-dashboard/seeker-dashboard.module').then(m => m.SeekerDashboardModule), canActivate: [authorizationGuard]},
-  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-  { path: '**', redirectTo: 'dashboard' }
+  {
+    path: 'account',
+    loadChildren: () => import('./account/account.module').then(module => module.AccountModule),
+    canActivate: [unauthorizationGuard]
+  },
+
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./dashboards/seeker-dashboard/seeker-dashboard.module').then(m => m.SeekerDashboardModule),
+    canActivate: [authorizationGuard]
+  },
+
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full' 
+  },
+
+  {
+    path: '**',
+    redirectTo: 'dashboard' 
+  }
 
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AccountService } from './account/account.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
- 
-  constructor(private accountService: AccountService){}
-  
+export class AppComponent implements OnInit, OnDestroy {
+
+  private subscriptions: Array<Subscription> = [];
+
+  constructor(private accountService: AccountService) { }
+
   public ngOnInit(): void {
     this.refreshUser();
-   } 
+  }
 
-  private refreshUser(){
+  public ngOnDestroy(): void {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
+  }
+
+  private refreshUser(): void {
     const jwt = this.accountService.getJwt();
-    if(jwt){
-      this.accountService.refreshUser(jwt).subscribe({
-        next: _ =>{},
-        error: _ =>{
-          this.accountService.logout();
-        }
-      });
+    if (jwt) {
+      this.subscriptions.push(
+        this.accountService.refreshUser(jwt).subscribe({
+          next: _ => { },
+          error: _ => {
+            this.accountService.logout();
+          }
+        })
+      )
     }
-    else{
-      this.accountService.refreshUser(null).subscribe();
+    else {
+      this.subscriptions.push(
+        this.accountService.refreshUser(null).subscribe()
+      )
     }
   }
 }
